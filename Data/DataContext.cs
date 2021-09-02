28582abb84f1f003bb456433e810e3c910a2e49b
EvandroBastos <evandrobastosviana@gmail.com>
2021-09-02 15:30:47 -0300

    primeiro commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+using System.Net.Mime;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+        public DbSet<Pessoa> pessoa { get; set; }
+    }
+}
